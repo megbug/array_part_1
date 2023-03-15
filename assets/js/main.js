cbd242c85dc9e767c94545fc0e44b68700fffedc
megbug <meghub@proton.me>
2023-03-15 14:51:15 +0100

    improved console.log

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,16 @@
 console.log("it works");
 
-// lvl_1_1
+// ======================================= lvl_1_1 =======================================
+
 const person = ["Megan", "28", "Leipzig"];
 const friends = ["Vanny", "Leo", "Max"];
 const favouriteFood = ["Sushi", "Sommerrollen", "Burrito"];
 
-// console.log(person , friends, favouriteFood);
+// console.log(person);
+// console.log(friends);
+// console.log(favouriteFood);
 
-// lvl_1_2
+// ======================================= lvl_1_2 =======================================
 
 // console.log(person[0]);
 // console.log(person[1]);
@@ -19,79 +22,91 @@ const favouriteFood = ["Sushi", "Sommerrollen", "Burrito"];
 // console.log(favouriteFood[1]);
 // console.log(favouriteFood[2]);
 
-// lvl_1_3
+// ======================================= lvl_1_3 =======================================
 
 // console.log(person.length);
 // console.log(friends.length);
 // console.log(favouriteFood.length);
 
-// lvl_1_4
+// ======================================= lvl_1_4 =======================================
 
 // console.log(person);
 // console.log(person.length);
-// console.log(person.push("Supercoderin"));
-// console.log(person.push("Rechtshänderin"));
+// person.push("Supercoderin");
+// person.push("Rechtshänderin");
 // console.log(person);
+// console.log(person.length);
 
 // console.log(friends);
 // console.log(friends.length);
-// console.log(friends.push("Ari", "Yasemin"));
+// friends.push("Ari", "Yasemin");
 // console.log(friends);
+// console.log(friends.length);
 
 // console.log(favouriteFood);
 // console.log(favouriteFood.length);
-// console.log(favouriteFood.push("Pizza", "Lasagne"));
+// favouriteFood.push("Pizza", "Lasagne");
 // console.log(favouriteFood);
+// console.log(favouriteFood.length);
 
-// lvl_1_5
-// console.log(person.push("Supercoderin"));
-// console.log(person.push("Rechtshänderin"));
+// ======================================= lvl_1_5 =======================================
+
+// person.push("Supercoderin");
+// person.push("Rechtshänderin");
 // console.log(person);
 // let lastValuePerson = person.pop();
 // console.log(lastValuePerson);
 // console.log(person);
 
-// console.log(friends.push("Ari", "Yasemin"));
+// friends.push("Ari", "Yasemin");
 // console.log(friends);
 // let lastValueFriends = friends.pop();
 // console.log(lastValueFriends);
 // console.log(friends);
 
-// console.log(favouriteFood.push("Pizza", "Lasagne"));
+// favouriteFood.push("Pizza", "Lasagne");
 // console.log(favouriteFood);
 // let lastValueFavouriteFood = favouriteFood.pop();
 // console.log(lastValueFavouriteFood);
 // console.log(favouriteFood);
 
-// lvl_1_6
+// ======================================= lvl_1_6 =======================================
 
+// person.push("Supercoderin");
+// console.log(person);
 // let firstValuePerson = person.shift();
 // console.log(firstValuePerson);
 // console.log(person);
 
-// lvl_1_7
+// ======================================= lvl_1_7 =======================================
 
+// friends.push("Ari", "Yasemin");
+// let lastValueFriends = friends.pop();
+// console.log(friends);
 // friends.unshift("Lukas", "Maurice");
 // console.log(friends);
 
+
+// favouriteFood.push("Pizza", "Lasagne");
+// let lastValueFavouriteFood = favouriteFood.pop();
+// console.log(favouriteFood);
 // favouriteFood.unshift("Pilze", "Eiscreme");
 // console.log(favouriteFood);
 
-// lvl_1_9 
+// ======================================= lvl_1_9 =======================================
 
 // const reiseZiele = ["Dänemark", "Portugal", "Frankreich", "Italien", "Schweden"];
 // console.log(reiseZiele);
 // console.log(reiseZiele.slice(2,4));
-// console.log(reiseZiele);
 
-// lvl_1_10
+// ======================================= lvl_1_10 =======================================
 
-const text = "It's okay, Bender. I like cooking too. Oh, you're a dollar naughtier than most. My fellow Earthicans, as I have explained in my book 'Earth in the Balance', and the much more popular 'Harry Potter and the Balance of Earth', we need to defend our planet against pollution. Also dark wizards.";
+// const text = "It's okay, Bender. I like cooking too. Oh, you're a dollar naughtier than most. My fellow Earthicans, as I have explained in my book 'Earth in the Balance', and the much more popular 'Harry Potter and the Balance of Earth', we need to defend our planet against pollution. Also dark wizards.";
 
-let firstSplit = text.split(".");
-let secondSplit = text.split(" ");
-let thirdSplit = text.split("");
+// let firstSplit = text.split(".");
+// let secondSplit = text.split(" ");
+// let thirdSplit = text.split("");
 
-console.log(firstSplit);
-console.log(secondSplit);
-console.log(thirdSplit);
+// console.log(firstSplit);
+// console.log(secondSplit);
+// console.log(thirdSplit);
